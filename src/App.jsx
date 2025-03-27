6e1313220a4012c3f79a3e4033955480eb9a7f7d
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-03-27 15:41:31 +0100

    add ArticleList component and initialize articles state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import ArticleList from "./components/ArticleList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [articles, setArticles] = useState([
+    { title: "Articolo 1" },
+    { title: "Articolo 2" },
+    { title: "Articolo 3" },
+  ]);
+  const [newTitle, setNewTitle] = useState("");
 
-  return <></>;
+  return (
+    <div className="App">
+      <ArticleList articles={articles} />
+    </div>
+  );
 }
 
 export default App;
